6c938a5ff69ebf4c274faa9cf46d07f42aabd741
edson <edsaraujoc@gmail.com>
2018-12-23 09:38:34 -0200

    Strings

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,23 +2,12 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        //Classes Numericas
-        Byte byte01 = new Byte((byte) 32);
-        System.out.println("Byte Class = " + byte01.byteValue());
+        //Objetos Strings
+        String nome = "Edson";
+        System.out.println("Seu nome e " + nome);
 
-        Short shor01 = new Short((short) 100);
-        System.out.println("Short class = " + shor01.shortValue());
-
-        Integer int01 = new Integer(1045);
-        System.out.println("Integer Class = " + int01.intValue());
-
-        Long long01 = new Long(10849L);
-        System.out.println("Long class = " + long01.longValue());
-
-        Float float01 = new Float(3.994F);
-        System.out.println("Float class = " + float01.floatValue());
-
-        Double double01 = new Double(3.123334);
-        System.out.println("Double class " + double01.doubleValue());
+        //A operacao basica para combinar strings chama-se concatenacao;
+        String s = "kile" + "meters";
+        System.out.println(s);
     }
 }
