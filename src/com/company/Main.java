13f3e713eb5724d2b580f7830959a3a772311bd9
edson <edsaraujoc@gmail.com>
2018-12-23 09:51:09 -0200

    Varias referencias para o mesmo objeto

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,6 +2,20 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        Counter counter = null; //Essa variavel nao aponta para nada
+        /* Pode haver varias referencias para um mesmo objeto */
+        Counter counter01 = new Counter();
+        Counter counter02 = counter01;
+
+        //Incremntar o contador em 10 vezes
+        for(int i = 0; i < 10; i++) {
+            counter01.incrementCount();
+        }
+
+        //Decrementar o contador em 5 vezes
+        for(int i = 0; i < 5; i++) {
+            counter02.decrementCount();
+        }
+
+        System.out.println("O valor do contador = " + counter01.getCount());
     }
 }
