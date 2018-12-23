fd0ad8ff44ac6783c46fc9acd2bfdd925aad8d9a
edson <edsaraujoc@gmail.com>
2018-12-23 09:32:40 -0200

    Classes Numericas

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,24 +2,23 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
+        //Classes Numericas
+        Byte byte01 = new Byte((byte) 32);
+        System.out.println("Byte Class = " + byte01.byteValue());
 
-        /* Em Java, um objeto e criado a partir de uma classe usando o operador new;
-            O operador new cria um novo objeto a partir de uma classe espeficiada e retorna
-            uma referencia para este objeto;
-         */
+        Short shor01 = new Short((short) 100);
+        System.out.println("Short class = " + shor01.shortValue());
 
-        //Declara a variavel c como sendo do tipo Counter; isto e, c pode se referir a qualquer objeto Counter;
-        Counter c;
+        Integer int01 = new Integer(1045);
+        System.out.println("Integer Class = " + int01.intValue());
 
-        //Declara a variavel d como sendo do tipo Couter; cria um novo objeto Counter e retorna uma referencia;
-        Counter d = new Counter(); //O operador = atribui a referencia ao novo objeto para variavel d;
+        Long long01 = new Long(10849L);
+        System.out.println("Long class = " + long01.longValue());
 
-        c = new Counter();
+        Float float01 = new Float(3.994F);
+        System.out.println("Float class = " + float01.floatValue());
 
-        //Atribui a referencia para o mesmo objeto que c (o objeto que d referenciava nao tem mais nenhuma variavel referenciando-o)
-        d = c;
-
-
-
+        Double double01 = new Double(3.123334);
+        System.out.println("Double class " + double01.doubleValue());
     }
 }
