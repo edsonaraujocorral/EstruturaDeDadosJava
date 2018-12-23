e77b6c7428938a5d2da6dce3eed5048058a9cc33
edson <edsaraujoc@gmail.com>
2018-12-23 09:45:47 -0200

    Variavel null

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,12 +2,6 @@ package com.company;
 
 public class Main {
     public static void main(String[] args) {
-        /* O operador new e usado para alocar espaco em memoria para o objeto e usar o
-            construtor do objeto para inicializar esse espaco.
-            O endereco deste espaco normalmente e atribuida para uma variavel de referencia.
-            Portando, uma variavel de referencia pode ser entendida como sendo um ponteiro para um
-            objeto...
-         */
-        Counter counter = new Counter();
+        Counter counter = null; //Essa variavel nao aponta para nada
     }
 }
