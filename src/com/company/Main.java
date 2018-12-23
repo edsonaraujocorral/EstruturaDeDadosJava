230b20b2efe56c76998a447f14128ebc63b9968e
edson <edsaraujoc@gmail.com>
2018-12-23 09:04:35 -0200

    Tipos primitivos do Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,14 +3,32 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        Counter primeiroContador = new Counter();
+        //Tipos Primitivos (que nao sao objetos)
 
-        //Incrementar o contador 10 vezes
-        for(int i = 0; i < 10; i++) {
-            primeiroContador.incrementCount();
-        }
+        //Valor booleano: True ou False;
+        boolean boolean01 = true;
+        boolean boolean02 = false;
 
-        System.out.println("O valor do contador = " + primeiroContador.getCount());
+        //Caracter Unicode de 16 bits
+        char char01 = 'C';
+
+        //byte: inteiro com sinal em complemento de dois de 8 bits
+        byte byte01 = 2;
+
+        //short: inteiro com sinal em complemento de dois de 16 bits
+        short shor01 = 29;
+
+        //int: inteiro com sinal em complemento de dois de 32 bits
+        int int01 = 231;
+
+        //long: inteiro com sinal em complemento de dois de 64 bits
+        long long01 = 23123;
+
+        //float: numero de ponto flutuante de 32 bits (IEEE 754-1985)
+        float float01 = 1.0f;
+
+        //double: numero de ponto flutuante de 64 bits (IEEE 754-1985)
+        double double01 = 123.12;
 
     }
 }
