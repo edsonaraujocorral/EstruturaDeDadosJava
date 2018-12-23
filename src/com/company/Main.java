cadfbd6b675e705d07c3795f429326b58f31bfa8
edson <edsaraujoc@gmail.com>
2018-12-23 09:09:58 -0200

    Implementado dos Tipos Primitivos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,30 +5,24 @@ public class Main {
     public static void main(String[] args) {
         //Tipos Primitivos (que nao sao objetos)
 
-        //Valor booleano: True ou False;
-        boolean boolean01 = true;
-        boolean boolean02 = false;
+        boolean flag = true;
+        char ch = 'A';
+        byte b = 12;
+        short s = 24;
+        int i =  257;
+        long l = 890L; //observe o uso do L
+        float f = 3.1415F; //observe o uso do F
+        double d = 2.1828;
 
-        //Caracter Unicode de 16 bits
-        char char01 = 'C';
+        System.out.println("flag = " + flag);
+        System.out.println("ch = " + ch);
+        System.out.println("b = " + b);
+        System.out.println("s = " + s);
+        System.out.println("i = " + i);
+        System.out.println("l = " + l);
+        System.out.println("f = " + f);
+        System.out.println("d = " + d);
 
-        //byte: inteiro com sinal em complemento de dois de 8 bits
-        byte byte01 = 2;
-
-        //short: inteiro com sinal em complemento de dois de 16 bits
-        short shor01 = 29;
-
-        //int: inteiro com sinal em complemento de dois de 32 bits
-        int int01 = 231;
-
-        //long: inteiro com sinal em complemento de dois de 64 bits
-        long long01 = 23123;
-
-        //float: numero de ponto flutuante de 32 bits (IEEE 754-1985)
-        float float01 = 1.0f;
-
-        //double: numero de ponto flutuante de 64 bits (IEEE 754-1985)
-        double double01 = 123.12;
 
     }
 }
