3723d9dfca4be1c93ed914ba4cd9b431889e13e9
edson <edsaraujoc@gmail.com>
2018-12-25 16:56:56 -0200

    Parametros

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -8,6 +8,11 @@ public class Main {
     public int soma(int x, int y) {
         return x + y;
     }
+    /* Todos os parametros em Java sao passados por valor, ou seja, sempre que se passa um parametro para um metodo,
+        uma copia do parametro e feita para o uso no contexto do corpo do metodo... O metodo pode alterar a copia,
+        mas nao o original... Quando se passa uma referencia do objeto como parametro para um metodo, entao essa
+        referencia e copiada da mesma forma...
+     */
     public static void main(String[] args) {
 
     }
