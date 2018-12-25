3511f973b4b997bda4296e08b8d8655577013e59
edson <edsaraujoc@gmail.com>
2018-12-25 17:16:55 -0200

    Metodo Main

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,7 +1,10 @@
 package com.company;
 
 public class Main {
-    
+
+    /* Classes que definem programas executaveis devem conter outro tipo especial de metodo
+        para uma classe - o metodo main..
+     */
     public static void main(String[] args) {
 
     }
