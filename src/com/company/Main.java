8160cc747808cf689a6fad83b4c12fb47b22a51d
edson <edsaraujoc@gmail.com>
2018-12-25 16:50:49 -0200

    Tipos de Retorno

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -2,17 +2,12 @@ package com.company;
 
 public class Main {
 
-    //A definicao de um metodo compreende duas partes: a assinatura, que define o nome e os parametros
-    //do metodo, e o corpo, que define o que o metodo realmente faz...
-    //Um metodo permite ao programador enviar uma mensagem para um objeto...
-    //A assinatura do metodo especifica como uma mensagem deve parecer.. e o corpo do metodo
-    //especifica o que o objeto ira fazer quando receber tal mensagem...
-
-    /* A lista de parametros e seus tipos declara as variaveis locais que correspondem aos valores
-        que sao passados como argumentos para o metodo...
+    /* Se o tipo de retorno e void, o metodo e chamado de procedimento; caso contrario, e chamado de
+        funcao...x
      */
-
-
+    public int soma(int x, int y) {
+        return x + y;
+    }
     public static void main(String[] args) {
 
     }
