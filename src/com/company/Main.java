22edd71daeb96a52605260f146d8817d64cdc0d1
edson <edsaraujoc@gmail.com>
2018-12-25 16:22:58 -0200

    Tipos Enumerados

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,29 +1,17 @@
 package com.company;
 
 public class Main {
+
+    public enum Day {MON, TUE, WED,THU, FRI, SAT, SUN};
     public static void main(String[] args) {
+        Day d = Day.MON;
+        System.out.println("Initially d is " + d);
 
-        /* variaveis de instancia ou campos; representam os dados associados com os objetos de uma classe.. */
+        d = Day.WED;
+        System.out.println("Then it is " + d);
 
-        /* O tipo da variavel pode ser tanto um tipo basico, indicando que a variavel armazena
-            valores daquele tipo, ou um nome de classe, indicando que a variavel e uma referencia para um
-            objeto desta classe...
-         */
-
-        /* O escopo (visibilidade) de uma variavel de instancia pode ser controlado pelo uso dos modificadores de variaveis*/
-        //Sintaxe: modificador tipo identificador = valor;
-
-        //Modificadores de escopo de variavel
-        //public: qualquer um pode acessar variaveis de instancia publicas;
-        //protected: apenas metodos do mesmo pacote ou subclasse podem acessar variaveis de instancia protegidas
-        //private: apenas metodos da mesma classe (excluindo metodos de uma subclasse) pode acessar variaveis de instancia privadas
-        /* Se nenhum dos modificadores for usado, entao a variavel de instancia e considerada amigavel */
-
-        //Modificadores de uso;
-        //static: e usada para declarar variaveis que e associada com a classe, e nao com instancias individuais daquela classe.
-        //final: uma variavel de instancia final e um tipo de variavel para o qual se deve atribuir um valor inicial,
-        //e para a qual, a partir de entao, nao e possivel atribuir um novo valor;
-
+        Day t = Day.valueOf("WED");
+        System.out.println("I say d and t are the same: " + (d == t));
 
     }
 }
