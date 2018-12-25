94399764c5d70df0a200f210eb7d5a42b9ecb4f6
edson <edsaraujoc@gmail.com>
2018-12-25 17:30:41 -0200

    Literais em Java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,19 +1,27 @@
 package com.company;
 
 public class Main {
-
-    /* Classes que definem programas executaveis devem conter outro tipo especial de metodo
-        para uma classe - o metodo main..
-     */
+    /* Variaveis e constantes sao usadas em expressoes para definir novos valores e para modificar variaveis */
     public static void main(String[] args) {
-        //Variaveis locais e blocos de codigos
-        {
-            double r;
-            int[] p1 = {3, 4};
-            int[] p2 = {4, 5};
-            int i = 31;
-            double e = 2.71828;
-        }
+
+        /* Um literal e qualquer valor "constante" que pode ser usado em uma atribuicao ou outro tipo
+            de expressao...
+         */
+        //Null := este e o unico literal que e um objeto e pode ser qualquer tipo de referencia..
+
+        //Booleano := true e false;
+
+        //Inteiro := int que corresponde um inteiro de 32 bits.. long que corresponde a inteiro longo de 64 bits e deve
+        //terminar por um L ou l, por exemplo: 156L ou -43l;
+
+        //Ponto Flutuante := o default para numeros de ponto flutuante, e ser do tipo double.
+        //para especificar um literal float, ele deve terminar por um "F" ou "f"..
+        //literais de ponto flutuante em notacao exponencial tambem sao aceitos, por exemplo: 3.25E2;
+
+        //Caractere := Um caractere e definido como um simbolo individual entre aspas simples..
+
+        //String := uma string e uma sequencia de caracteres entre aspas duplas.
+
 
     }
 }
