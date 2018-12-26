319cbbe21e4fb8cb7939bc5c8e5bb90f158066e9
edson <edsaraujoc@gmail.com>
2018-12-26 14:08:25 -0200

    Saida Padrao Simples

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,70 +1,13 @@
 package com.company;
 
 public class Main {
-    //Somatoria de todos os elementos de um Array
-    public static int sum(int[] a) {
-        int total = 0;
-        for(int i = 0; i < a.length; i++) {
-            total += a[i];
-        }
-        return total;
-    }
-
-    //Contador do Numero de frequencia de um elemento de um Array
-    public static int findCount(int[] a, int k) {
-        int count = 0;
-        for(int i = 0; i < a.length; i++) {
-            if(a[i] == k) {
-                count++;
-            }
-        }
-
-        return count;
-    }
-    //Encontra o menor elemento
-    public static int findMinor(int[] a) {
-        int j = 0;
-        for(int i = 0; i < a.length; i++) {
-            if(a[i] < a[j]) {
-                j = i;
-            }
-        }
-
-        return j;
-    }
-
     public static void main(String[] args) {
-        //Arrays := sao colecoes numeradas de variaveis do mesmo tipo..
-        //Cada variavel ou celula em um arranjo tem um indi
-        /* Cada variavel ou celula em um array tem um indice,
-         */
-        int[] lista = {10, 20, 10, 40, 50, 60};
-        int total = sum(lista);
-        System.out.println("Total: " + total);
-
-        int frequencia = findCount(lista, 10);
-        System.out.println("O Numero de vezes que o numero 10 apareceu na Lista e " + frequencia);
-
-        int menor = findMinor(lista);
-        System.out.println("O menor elemento: " + lista[menor]);
-
-        //E muito perigoso tentar indexar em um array a um fora do intervalo de [0, a.length - 1]
-        //Tal referencia e dita "Fora de Faixa"
-        /* Se um indice de um array esta fora de faixa, o ambiente de execucao de Java sinaliza uma condicao
-            de erro chamada ArrayIndexOutOfBoundsException
-         */
-        //Declaracao de Arrays
-        int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
-
-        double[] a = new double[10];
-
-        for(int i = 0; i < a.length; i++) {
-            a[i] = 1.0;
-        }
-
-        for(int i = 0; i < a.length; i++) {
-            System.out.println(a[i]);
-        }
-
+        //Entrada e Saida Simples
+        //System.out e um instancia da classe java.io.PrintStream
+        System.out.print("Java values: ");
+        System.out.print(3.1415);
+        System.out.print(',');
+        System.out.print(15);
+        System.out.print(" (double, char, int).");
     }
 }
