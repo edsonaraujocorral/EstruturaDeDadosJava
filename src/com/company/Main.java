7049e7611d1e11b9434864e8ed258a8253406d1b
edson <edsaraujoc@gmail.com>
2018-12-26 12:10:13 -0200

    Array e Funcao soma todos os elementos

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,20 +1,21 @@
 package com.company;
 
 public class Main {
-    public static void main(String[] args) {
-        /* break := e usado para sair do bloco internamente mais aninhado dos comandos:
-            switch, for, while, do-while.
-         */
-        int j = 0;
-
-        for(int i = 0; i < 10; i++) {
-            j += i;
-            if(j == 5) {
-               break;
-            }
+    public static int sum(int[] a) {
+        int total = 0;
+        for(int i = 0; i < a.length; i++) {
+            total += a[i];
         }
-       System.out.println("Test");
-
+        return total;
+    }
+    public static void main(String[] args) {
+        //Arrays := sao colecoes numeradas de variaveis do mesmo tipo..
+        //Cada variavel ou celula em um arranjo tem um indi
+        /* Cada variavel ou celula em um array tem um indice,
+         */
+        int[] lista = {10, 20, 30, 40, 50, 60};
+        int total = sum(lista);
+        System.out.println("Total: " + total);
 
     }
 }
