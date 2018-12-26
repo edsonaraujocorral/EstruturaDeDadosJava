98acb0e3943c078561cf4f155d44963a79b7d6a7
edson <edsaraujoc@gmail.com>
2018-12-26 12:00:42 -0200

    Break command

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,32 +1,20 @@
 package com.company;
 
 public class Main {
-    /* Se um metodo Java e declarado como tipo de retorno void, entao fluxo de controle
-        retorna quando encontra a ultima linha de codigo do metodo ou quando encontra um
-        comando return sem argumentos...
-     */
-    private static void sum(int x, int y) {
-        System.out.println("The sum is: " + (x + y));
-    }
-
-    /* Entretando, se um metodo e declarado com um tipo de retorno, ele e uma funcao
-        e devera terminar retornando o valor da funcao como um argumento de comando return.
-     */
-    public static boolean checkBDay(int date) {
-        if(date == 20) {
-            return true;
-        }
-        return false;
-    } //O comando return deve ser o ultimo comando executado em uma funcao...
-
     public static void main(String[] args) {
-        //Expressoes explicitas de controle de fluxo..
-        sum(10, 20);
-        boolean setData = checkBDay(20);
-        if(setData) {
-            System.out.println("Data is true");
-        } else {
-            System.out.println("Data is false");
+        /* break := e usado para sair do bloco internamente mais aninhado dos comandos:
+            switch, for, while, do-while.
+         */
+        int j = 0;
+
+        for(int i = 0; i < 10; i++) {
+            j += i;
+            if(j == 5) {
+               break;
+            }
         }
+       System.out.println("Test");
+
+
     }
 }
