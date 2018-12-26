8c557ed9aaab1fb175a647baf904526b6b6587d4
edson <edsaraujoc@gmail.com>
2018-12-26 12:39:27 -0200

    Working with Arrays

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -21,7 +21,17 @@ public class Main {
 
         return count;
     }
+    //Encontra o menor elemento
+    public static int findMinor(int[] a) {
+        int j = 0;
+        for(int i = 0; i < a.length; i++) {
+            if(a[i] < a[j]) {
+                j = i;
+            }
+        }
 
+        return j;
+    }
 
     public static void main(String[] args) {
         //Arrays := sao colecoes numeradas de variaveis do mesmo tipo..
@@ -34,6 +44,27 @@ public class Main {
 
         int frequencia = findCount(lista, 10);
         System.out.println("O Numero de vezes que o numero 10 apareceu na Lista e " + frequencia);
+
+        int menor = findMinor(lista);
+        System.out.println("O menor elemento: " + lista[menor]);
+
+        //E muito perigoso tentar indexar em um array a um fora do intervalo de [0, a.length - 1]
+        //Tal referencia e dita "Fora de Faixa"
+        /* Se um indice de um array esta fora de faixa, o ambiente de execucao de Java sinaliza uma condicao
+            de erro chamada ArrayIndexOutOfBoundsException
+         */
+        //Declaracao de Arrays
+        int[] primes = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29};
+
+        double[] a = new double[10];
+
+        for(int i = 0; i < a.length; i++) {
+            a[i] = 1.0;
+        }
+
+        for(int i = 0; i < a.length; i++) {
+            System.out.println(a[i]);
+        }
 
     }
 }
