369692c7c826364a567197139bdce81c5db3213d
edson <edsaraujoc@gmail.com>
2018-12-26 14:49:53 -0200

    Implementacao do CreditCard

---
+++ src/com/company/CreditCard.java
@@ -0,0 +1,46 @@
+package com.company;
+
+public class CreditCard {
+    //Variaveis de Instancia
+    private String number;
+    private String name;
+    private String bank;
+    private double balance;
+    private int limit;
+
+    //Construtor
+    public CreditCard(String number, String name, String bank, double balance, int limit) {
+        this.number = number;
+        this.name = name;
+        this.bank = bank;
+        this.balance = balance;
+        this.limit = limit;
+    }
+
+    //Metodos de Acesso
+    public String getNumber() { return this.number; }
+    public String getName() { return this.name; }
+    public String getBank() { return this.bank; }
+    public double getBalance() { return this.balance; }
+    public int getLimit() { return this.limit; }
+
+    //Metodos de acao
+    public boolean chargelt(double price) {
+        if(price + this.balance > (double)this.limit) {
+            return false; //Na ha dinheiro sufuciente para debitar
+        }
+
+        this.balance += price;
+        return true;//Debito foi efetuado..
+    }
+    public void makePayment(double payment) {
+        this.balance -= payment;
+    }
+    public static void printCard(CreditCard card) {
+        System.out.println("Number: " + card.getNumber());
+        System.out.println("Name: " + card.getName());
+        System.out.println("Bank: " + card.getBank());
+        System.out.println("Balance: " + card.getBalance());
+        System.out.println("Limit: " + card.getLimit());
+    }
+}
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -5,21 +5,23 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        CreditCard wallet[] = new CreditCard[10];
+        wallet[0] = new CreditCard("5391 0375 9387 5309",
+                "John Bowman", "California Savings", 0.0, 2500);
+        wallet[1] = new CreditCard("3485 0399 3395 1957",
+                "Jon Bowman", "California Federal", 0.0, 3500);
 
-        Scanner s = new Scanner(System.in);
-        System.out.print("Digite um limite para lista: ");
-        int limiteLista = s.nextInt();
-
-        int[] lista = new int[limiteLista];
-
-        for(int i = 0; i < lista.length; i++) {
-            System.out.print("Digite o " + i + " valor para lista: ");
-            int value = s.nextInt();
-            lista[i] = value;
+        for(int i = 1; i <= 16; i++) {
+            wallet[0].chargelt((double) i);
+            wallet[1].chargelt(2.0*i);
+        }
+        for(int i = 0; i < 2; i++) {
+            CreditCard.printCard(wallet[i]);
+            while(wallet[i].getBalance() > 100.0){
+                wallet[i].makePayment(100.00);
+                System.out.println("New balance = " + wallet[i].getBalance());
+            }
         }
 
-        for(int i = 0; i < lista.length; i++) {
-            System.out.print(lista[i] + " ");
-        }
     }
 }
