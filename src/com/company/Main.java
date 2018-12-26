6ab874a99b7ee15a4b3feaf62af3bc628d70c354
edson <edsaraujoc@gmail.com>
2018-12-26 14:25:53 -0200

    Criando uma lista dinamica

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,25 +1,25 @@
 package com.company;
+
 import java.io.*;
 import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
-        //Entrada e Saida Simples
-        //System.out e um instancia da classe java.io.PrintStream
-        System.out.print("Java values: ");
-        System.out.print(3.1415);
-        System.out.print(',');
-        System.out.print(15);
-        System.out.print(" (double, char, int).");
 
-        //Entrada Simples Usando a classe java.util.Scanner;
-        //System.in := E um objeto usado para executar a entrada de dados a partir da janela de console Java.
         Scanner s = new Scanner(System.in);
-        System.out.println("Enter your age in years: ");
-        double age = s.nextDouble();
-        System.out.println("Enter your maximum heart rate: ");
-        double rate = s.nextDouble();
-        double fb = (rate - age) * 0.65;
-        System.out.println("Your target fat burning heart rate is " + fb + ".");
+        System.out.print("Digite um limite para lista: ");
+        int limiteLista = s.nextInt();
+
+        int[] lista = new int[limiteLista];
+
+        for(int i = 0; i < lista.length; i++) {
+            System.out.print("Digite o " + i + " valor para lista: ");
+            int value = s.nextInt();
+            lista[i] = value;
+        }
+
+        for(int i = 0; i < lista.length; i++) {
+            System.out.print(lista[i] + " ");
+        }
     }
 }
