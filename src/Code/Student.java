bc4cd385f09eba9a365c7c2d42be8582bf8ba99c
edson <edsaraujoc@gmail.com>
2018-12-26 15:37:21 -0200

    Definindo constantes

---
+++ src/Code/Student.java
@@ -0,0 +1,16 @@
+package Code;
+
+/**
+ * @author Edson
+ */
+public class Student {
+   //Constantes
+   public static final int MIN_CREDITS = 12;
+   public static final int MAX_CREDITS = 24;
+   public static final int FRESHMAN = 1;
+   public static final int SOPHOMORE = 2;
+   public static final int JUNIOR = 3;
+   public static final int SENIOR = 4;
+
+
+}
