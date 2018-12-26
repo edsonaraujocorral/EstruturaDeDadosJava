89443bcef4649b2490fc454f6624ea218d11cbb7
edson <edsaraujoc@gmail.com>
2018-12-26 14:16:23 -0200

    Fat burn heart Implements

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,4 +1,6 @@
 package com.company;
+import java.io.*;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +11,15 @@ public class Main {
         System.out.print(',');
         System.out.print(15);
         System.out.print(" (double, char, int).");
+
+        //Entrada Simples Usando a classe java.util.Scanner;
+        //System.in := E um objeto usado para executar a entrada de dados a partir da janela de console Java.
+        Scanner s = new Scanner(System.in);
+        System.out.println("Enter your age in years: ");
+        double age = s.nextDouble();
+        System.out.println("Enter your maximum heart rate: ");
+        double rate = s.nextDouble();
+        double fb = (rate - age) * 0.65;
+        System.out.println("Your target fat burning heart rate is " + fb + ".");
     }
 }
