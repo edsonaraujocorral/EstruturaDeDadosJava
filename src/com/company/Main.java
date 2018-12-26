11cecce7b4418aff404c994644de4352f10b622e
edson <edsaraujoc@gmail.com>
2018-12-26 11:18:07 -0200

    Switch

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,6 +1,9 @@
 package com.company;
 
 public class Main {
+
+    private enum Day {MON, TUE, WED, THU, FRI, SAT, SUN};
+
     public static void main(String[] args) {
         //Controle de Fluxo
         /* Os valores testados por um comando if devem ser uma
@@ -16,5 +19,26 @@ public class Main {
             System.out.println("X e menor que 10 e nao e igual a 15");
         }
 
+        Day d = Day.MON;
+        switch(d) {
+            case MON:
+                System.out.println("This is tough...");
+                break;
+            case TUE:
+                System.out.println("This is getting better...");
+                break;
+            case WED:
+                System.out.println("Half way there...");
+                break;
+            case THU:
+                System.out.println("I can see the light...");
+                break;
+            case FRI:
+                System.out.println("Now we ara talking...");
+                break;
+            default:
+                System.out.println("Day off!!");
+                break;
+        }
     }
 }
